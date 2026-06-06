20b04f552cdc4d6a26142846e9270ddc05db8f23
Richard Clacksman <kiml42a@gmail.com>
2026-06-06 18:18:53 +0100

    Car movement forwards.

---
+++ Assets/Behaviours/CarMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarMovement : MonoBehaviour
+{
+    public float moveForce = 20f;
+
+    private Rigidbody rb;
+    private InputAction moveAction;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveAction = InputSystem.actions.FindAction("Player/Move");
+    }
+
+    void OnEnable() => moveAction.Enable();
+    void OnDisable() => moveAction.Disable();
+
+    void FixedUpdate()
+    {
+        float forward = moveAction.ReadValue<Vector2>().y;
+        rb.AddForce(transform.forward * forward * moveForce);
+    }
+}
