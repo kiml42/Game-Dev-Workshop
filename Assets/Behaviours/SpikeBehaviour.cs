23543c652e099b0dd66d5171eea859b08c0bc405
Richard Clacksman <kiml42a@gmail.com>
2026-06-25 12:05:05 +0100

    added background objects

--- Assets/Behaviours/SpikeBehaviour.cs
+++ Assets/Behaviours/SpikeBehaviour.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpikeBehaviour : MonoBehaviour
 {
     private Vector3 _cubeStartPosition;
     private Rigidbody _cubeRigidbody;
+    private Renderer _cubeRenderer;
 
     void Start()
     {
@@ -11,6 +13,7 @@ public class SpikeBehaviour : MonoBehaviour
 
         var cube = FindFirstObjectByType<JumpBehaviour>();
         _cubeRigidbody = cube.GetComponent<Rigidbody>();
+        _cubeRenderer = cube.GetComponent<Renderer>();
         _cubeStartPosition = _cubeRigidbody.transform.position;
     }
 
@@ -18,7 +21,19 @@ public class SpikeBehaviour : MonoBehaviour
     {
         if (other.GetComponent<JumpBehaviour>() == null) return;
 
+        StartCoroutine(ResetSequence());
+    }
+
+    IEnumerator ResetSequence()
+    {
+        _cubeRenderer.enabled = false;
         _cubeRigidbody.linearVelocity = Vector3.zero;
+        Time.timeScale = 0f;
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        Time.timeScale = 1f;
         _cubeRigidbody.transform.position = _cubeStartPosition;
+        _cubeRenderer.enabled = true;
     }
 }
