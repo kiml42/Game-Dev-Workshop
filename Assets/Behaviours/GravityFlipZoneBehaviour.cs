02e37f66fa3d0dd34813f832eadda280f628090e
Richard Clacksman <kiml42a@gmail.com>
2026-06-25 12:49:11 +0100

    fixing grav portal 1

--- Assets/Behaviours/GravityFlipZoneBehaviour.cs
+++ Assets/Behaviours/GravityFlipZoneBehaviour.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class GravityFlipZoneBehaviour : MonoBehaviour
 {
-    void Start()
-    {
-        GetComponent<Collider>().isTrigger = true;
-
-        var rb = gameObject.AddComponent<Rigidbody>();
-        rb.isKinematic = true;
-        rb.useGravity = false;
-    }
-
     void OnTriggerEnter(Collider other)
     {
         var jump = other.GetComponent<JumpBehaviour>();
