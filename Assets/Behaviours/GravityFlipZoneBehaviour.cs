dbd0b1f476d09a2a3d3abc6315e76c83aa2f600b
Richard Clacksman <kiml42a@gmail.com>
2026-06-25 12:54:28 +0100

    fixing grav portal 3

--- Assets/Behaviours/GravityFlipZoneBehaviour.cs
+++ Assets/Behaviours/GravityFlipZoneBehaviour.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class GravityFlipZoneBehaviour : MonoBehaviour
 {
+    private float _lastFlipTime = -10f;
+
     void OnTriggerEnter(Collider other)
     {
+        if (Time.unscaledTime - _lastFlipTime < 1f) return;
+
         var jump = other.GetComponent<JumpBehaviour>();
-        if (jump != null) jump.FlipGravity();
+        if (jump == null) return;
+
+        jump.FlipGravity();
+        _lastFlipTime = Time.unscaledTime;
     }
 }
