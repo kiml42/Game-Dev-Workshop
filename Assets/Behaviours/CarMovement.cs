051f1efe25e67398b006f5dc796420819c0d5329
Richard Clacksman <kiml42a@gmail.com>
2026-07-15 18:11:51 +0100

    Made the car jump

--- Assets/Behaviours/CarMovement.cs
+++ Assets/Behaviours/CarMovement.cs
@@ -5,18 +5,39 @@ public class CarMovement : MonoBehaviour
 {
     public float moveForce = 20f;
     public float turnTorque = 10f;
+    public float jumpForce = 5f;
+    public float groundCheckDistance = 1f;
 
     private Rigidbody rb;
     private InputAction moveAction;
+    private InputAction jumpAction;
+    private bool jumpRequested;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
         moveAction = InputSystem.actions.FindAction("Player/Move");
+        jumpAction = InputSystem.actions.FindAction("Player/Jump");
     }
 
-    void OnEnable() => moveAction.Enable();
-    void OnDisable() => moveAction.Disable();
+    void OnEnable()
+    {
+        moveAction.Enable();
+        jumpAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
+    }
+
+    void Update()
+    {
+        // Capture the button press in Update so we don't miss it between physics steps.
+        if (jumpAction.WasPressedThisFrame() && IsGrounded())
+            jumpRequested = true;
+    }
 
     void FixedUpdate()
     {
@@ -25,5 +46,23 @@ public class CarMovement : MonoBehaviour
         float speed = forward < 0 ? moveForce * 0.75f : moveForce;
         rb.AddForce(transform.forward * forward * speed);
         rb.AddTorque(transform.up * input.x * turnTorque);
+
+        if (jumpRequested)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpRequested = false;
+        }
+    }
+
+    // Grounded if something other than the car's own body is just below us.
+    private bool IsGrounded()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, Vector3.down, groundCheckDistance);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.rigidbody != rb)
+                return true;
+        }
+        return false;
     }
 }
